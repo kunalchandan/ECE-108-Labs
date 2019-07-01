2865d6800197904288d11b1dc1d86510aade7638
Kunal Chandan <kunal_chandan@outlook.com>
2019-07-01 18:27:16 -0400

    Half Working Lab2

--- ECE.108.S19-G008/Lab2/main.cpp
+++ ECE.108.S19-G008/Lab2/main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <fstream>
+#include <sstream>
 #include <stdio.h>
+#include <vector>
 #include <string>
 
 using std::cout;
@@ -14,26 +16,94 @@ int IX(int x, int y, int xLen){
     return (x*xLen) + y;
 }
 
-int main(int argc, char *argv[]) {
+int* read_expression(const string fileName){
     ifstream dimacs;
-    int numVars;
-    int numClauses;
-    char thisChar;
-    string fileName = argv[1];
-
+    int num_vars;
+    int num_clauses;
+    char this_char;
     dimacs.open(fileName);
     if (!dimacs) {
         cerr << "Unable to open the file" << endl;
         exit(1);
     }
 
-    dimacs >> numVars >> numClauses;
-    cout << "Variables: " << numVars << " Clauses: " << numClauses << endl;
+    dimacs >> num_vars >> num_clauses;
+    cout << "Variables: " << num_vars << " Clauses: " << num_clauses << endl;
+    //don't cares are stored as 0, true = 1, false = -1
+    int* expr = new int [num_clauses*num_vars]{0};
     cout << "Reading Expression..." << endl;
 
-    int* all = new int[numClauses*numVars]{};
-    int numTests = 0;
-    int* tests = new int[numTests*numVars]{};
+    char prev_char = '1';
+    int col = 0;
+    while (dimacs >> this_char) {
+        if (this_char == '0'){
+            cout << endl;
+            col ++;
+
+        }else if (this_char == ' '){
+            continue;
+        }else if(this_char == '-'){
+            prev_char = '-';
+
+        }else{
+            int var =  this_char - '0';
+            if (prev_char == '-'){
+                var = var * -1;
+            }
+            int index = abs(var) - 1;
+            expr[IX(col, index, num_clauses)] = ((var > 0) ? 1: -1);
+            cout << ((var > 0)? "1": "-1");
+            prev_char = this_char;
+        }
+    }
+    cout << endl;
+    dimacs.close();
+    return expr;
+}
+
+std::vector<std::vector<int>>* read_tests(){
+    auto *tests = new std::vector<std::vector<int>>();
+    auto *line = new std::vector<int>();
+    int c_num;
+    string input;
+    std::getline(cin, input);
+    std::istringstream iss(input);
+
+    while(iss >> c_num){
+        if(c_num == 0){
+            tests->push_back(*line);
+            line->clear();
+        }else{
+            line->push_back(c_num);
+        }
+    }
+    return tests;
+}
+
+int main(int argc, char *argv[]) {
+    ifstream dimacs;
+    int numVars;
+    int numClauses;
+    string fileName = argv[1];
+
+    dimacs.open(fileName);
+    if (!dimacs) {
+        cerr << "Unable to open the file " << fileName << endl;
+        exit(1);
+    }
+
+    dimacs >> numVars >> numClauses;
+    dimacs.close();
+
+    cout << "Variables: " << numVars << " Clauses: " << numClauses << endl;
+
+    // Size = [numClauses*numVars]
+    int* all = read_expression(fileName);
+
+    // Size = [numTests*numVars]
+    std::vector<std::vector<int>> tests = *read_tests();
+    int numTests = (int)(tests.size());
+
     for(int t = 0; t < numTests; t++){
 
         // Clone the expression array
@@ -42,7 +112,7 @@ int main(int argc, char *argv[]) {
 
         // Invert any not-ed values
         for(int x = 0; x < numVars; x++){
-            if(tests[IX(t, x, numVars)] == -1){
+            if(tests.at(t).at(x) <= -1){
                 for(int j = 0; j < numClauses; j++){
                     // Convert True to False and vice-versa
                     clone[IX(j, x, numVars)] *= -1;
@@ -96,7 +166,7 @@ int main(int argc, char *argv[]) {
                 }
             }
         }
-        cout << final << endl;
+        cout << "Expression Value for Test " << t+1 << ": " << final << endl;
     }
 
     return 0;
