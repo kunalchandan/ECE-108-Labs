749a9b7f9b60fddb5e2d9324a17f1be77e1dd5ec
Quancong Ni <quancong.ni@uwaterloo.ca>
2019-07-23 11:17:44 -0400

    fixed stack_input() but it still doesn't really work so i'll fix it later

--- G007-G008/Lab4/team/shared.h
+++ G007-G008/Lab4/team/shared.h
@@ -13,10 +13,11 @@ void stack_input(){
 	srand(time(NULL));
 	for(int i = 0; i < stack_size; ++i){
 		input_curr = rand() % 111;
-		if(input_curr == 9 | input_curr == 18 | input_curr == 27){	
+		if(input_curr == 9 || input_curr == 18 || input_curr == 27){	
 			// if the input is 9, 18, or 27 then increase stack size by prev input
 		    thiccc_stack[i] = input_curr;
-		    thiccc_stack.resize(stack_size += input_prev);
+		    thiccc_stack.resize(stack_size + input_prev);
+		    stack_size += input_prev;
 		    input_prev = input_curr;
 		}else if(input_curr == 13){
 			// if the input is 13 then decrease stack size by prev input
@@ -25,8 +26,10 @@ void stack_input(){
 			if(stack_size - input_prev < 0){
 			// if the stack size decreases below zero, set straight to 0
 				thiccc_stack.resize(0);
+				stack_size = 0;
 			}else{
-				thiccc_stack.resize(stack_size -= input_prev);
+				thiccc_stack.resize(stack_size - input_prev);
+				stack_size -= input_prev;
 			}
 		    input_prev = input_curr;
 		}else{
