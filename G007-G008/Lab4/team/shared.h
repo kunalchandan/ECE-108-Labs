e5785daa3607157363671867a7124890500ba6e2
Quancong Ni <quancong.ni@uwaterloo.ca>
2019-07-23 10:18:04 -0400

    commented and fixed formatting for stack_input() in shared.h

--- G007-G008/Lab4/team/shared.h
+++ G007-G008/Lab4/team/shared.h
@@ -12,17 +12,18 @@ int input_prev{0}, input_curr{0}, stack_size{500};					// input_prev is initiali
 void stack_input(){
 	for(int i = 0; i < stack_size; ++i){
 		input_curr = rand() % 111;
-		if(input_curr == 9 | input_curr == 18 | input_curr == 27){
-		    thiccc_stack[i] = input_curr;
-		    thiccc_stack.resize(stack_size += input_prev);
-		    input_prev = input_curr;
-		}else if(input_curr == 13){
-		    thiccc_stack[i] = input_curr;
-		    thiccc_stack.resize(stack_size -= input_prev);
-		    input_prev = input_curr;
-		}else{
-		    thiccc_stack[i] = input_curr;
-		    input_prev = input_curr;
+		if(input_curr == 9 | input_curr == 18 | input_curr == 27){	// if the input is 9, 18, or 27
+		    thiccc_stack[i] = input_curr;							// add the number to the back of the stack
+		    thiccc_stack.resize(stack_size += input_prev);			// and increase the stack size by the previous input
+		    input_prev = input_curr;								// cache the current input as previous input for the next input
+		}else if(input_curr == 13){									// if the input is 27
+		    thiccc_stack[i] = input_curr;							// add the number to the back of the stack
+		    thiccc_stack.resize(stack_size -= input_prev);			// and decrease the stack size by the previous input
+			// note that if i exceeds stack_size, the for loop will immediately end and all contents unfit for the stack will be truncated
+		    input_prev = input_curr;								// cache the current input as previous input for the next input
+		}else{														// if the input is not important
+		    thiccc_stack[i] = input_curr;							// add the number to the back of the stack
+		    input_prev = input_curr;								// cache the current input as previous input for the next input
 		}
 	}
 }
