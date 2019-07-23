b6348710df04e30e8312b6f0e0a3b74f7eb317a2
Quancong Ni <quancong.ni@uwaterloo.ca>
2019-07-23 10:28:45 -0400

    fixed random seed for stack_input(), formatting

--- G007-G008/Lab4/team/shared.h
+++ G007-G008/Lab4/team/shared.h
@@ -10,20 +10,24 @@ int input_prev{0}, input_curr{0}, stack_size{500};					// input_prev is initiali
 
 
 void stack_input(){
+	srand(time(NULL));
 	for(int i = 0; i < stack_size; ++i){
 		input_curr = rand() % 111;
-		if(input_curr == 9 | input_curr == 18 | input_curr == 27){	// if the input is 9, 18, or 27
-		    thiccc_stack[i] = input_curr;							// add the number to the back of the stack
-		    thiccc_stack.resize(stack_size += input_prev);			// and increase the stack size by the previous input
-		    input_prev = input_curr;								// cache the current input as previous input for the next input
-		}else if(input_curr == 13){									// if the input is 27
-		    thiccc_stack[i] = input_curr;							// add the number to the back of the stack
-		    thiccc_stack.resize(stack_size -= input_prev);			// and decrease the stack size by the previous input
+		if(input_curr == 9 | input_curr == 18 | input_curr == 27){	
+			// if the input is 9, 18, or 27 then increase stack size by prev input
+		    thiccc_stack[i] = input_curr;
+		    thiccc_stack.resize(stack_size += input_prev);
+		    input_prev = input_curr;
+		}else if(input_curr == 13){
+			// if the input is 13 then decrease stack size by prev input
 			// note that if i exceeds stack_size, the for loop will immediately end and all contents unfit for the stack will be truncated
-		    input_prev = input_curr;								// cache the current input as previous input for the next input
-		}else{														// if the input is not important
-		    thiccc_stack[i] = input_curr;							// add the number to the back of the stack
-		    input_prev = input_curr;								// cache the current input as previous input for the next input
+		    thiccc_stack[i] = input_curr;
+		    thiccc_stack.resize(stack_size -= input_prev);
+		    input_prev = input_curr;
+		}else{
+			// if the input is not important then input to stack as is
+		    thiccc_stack[i] = input_curr;
+		    input_prev = input_curr;
 		}
 	}
 }
