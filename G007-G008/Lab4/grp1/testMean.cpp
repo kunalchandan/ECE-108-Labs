3b364594872786b165c9242f77d9f1c80cde0c45
Piggosu <quancong.ni@uwaterloo.ca>
2019-07-28 04:17:22 -0400

    did not properly commit last time, trying again: created testMean.cpp

--- G007-G008/Lab4/grp1/testMean.cpp
+++ G007-G008/Lab4/grp1/testMean.cpp
@@ -1,5 +1,40 @@
-#include "Mean.h"
-#include <string>
+#include "testMean.h"
+#include "../team/shared.h"
+#include <iostream>
 
-bool testMean();
-bool testMean(unsigned int input_size,vector<int> input_set);
+int main();
+
+bool testMean(){
+	bool output = true;
+	Mean testing;
+	
+	if(testing.get_mean != 0){
+		output = false;
+	}
+	
+	if(testing.get_size != 0){
+		output = false;
+	}
+	
+	return output;
+}
+
+bool testMean(unsigned int input_size, std::vector<int> input_set){
+	bool output = true;
+	Mean testing;
+	testing.calculated(unsigned int input_size, std::vector<int> input_set);
+	//not sure if this is how we're supposed to implement the function, going to confirm later today -- Leo
+}
+
+int main(){
+	bool test1 = testMean();
+	bool test2 = testMean(unsigned int input_size,vector<int> input_set);
+	
+	if(test1 && test2){
+		std::cout << "Test success: Mean";
+	}else{
+		std::cout << "Test failed: Mean"
+	}
+	
+	return 0;
+}
